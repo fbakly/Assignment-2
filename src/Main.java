6c700084460499678ad897b8ee1b9c490534a67d
fbakly <fbakly@users.noreply.github.com>
2019-06-02 16:55:58 +0200

    Added the RssReader library

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 public class Main {
     public static void main(String[] args) {
-
+        var tf= new TwitterFactory();
+        var twitter = tf.getInstance();
+        try {
+            //twitter.updateStatus("First Bot Post");
+            var message = twitter.sendDirectMessage(580637424, "My twitter bot's first direct message");
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
     }
 }
