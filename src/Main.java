721b72098b5b03650c349973f0fb56503eff1af6
fbakly <fbakly@users.noreply.github.com>
2019-06-04 16:40:50 +0200

    Figured out how to read messages

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import com.apptastic.rssreader.RssReader;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
+import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        var tf= new TwitterFactory();
+
+        var tf = new TwitterFactory();
         var twitter = tf.getInstance();
         try {
-            //twitter.updateStatus("First Bot Post");
-            var message = twitter.sendDirectMessage(580637424, "My twitter bot's first direct message");
+            var messagesSent = twitter.getDirectMessages(1);
+            for (var index : messagesSent) {
+                var hashtags = index.getHashtagEntities();
+                System.out.print(index.getRecipientId() + " got a message from " + index.getSenderId() + " " + index.getText());
+                for (var hashtag : hashtags) {
+                    System.out.print(hashtag.getText() + " ");
+                }
+                System.out.println();
+            }
         } catch (TwitterException te) {
             te.printStackTrace();
         }
